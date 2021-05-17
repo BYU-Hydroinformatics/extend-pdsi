49491390e4ccb87ca678217375637e6fc6b6aacc
Riley Hales <rileyhales1@gmail.com>
2021-05-17 15:11:06 -0600

    sh scripts queue resample jobs

---
+++ gldas-data/run_resample.sh
@@ -0,0 +1,3 @@
+python resample_gldas.py group1/ half-degree-data/ log1.log &
+python resample_gldas.py group2/ half-degree-data/ log2.log &
+python resample_gldas.py group3/ half-degree-data/ log3.log &
---
+++ gldas-data/split_data_to_groups.sh
@@ -0,0 +1,18 @@
+mkdir group1
+mkdir group2
+mkdir group3
+mv original-data/*194* group1/
+mv original-data/*195* group1/
+mv original-data/*196* group1/
+mv original-data/*197* group2/
+mv original-data/*198* group2/
+mv original-data/*1991* group2/
+mv original-data/*1992* group2/
+mv original-data/*1993* group2/
+mv original-data/*1994* group2/
+mv original-data/*1995* group2/
+mv original-data/*1996* group3/
+mv original-data/*1997* group3/
+mv original-data/*1998* group3/
+mv original-data/*1999* group3/
+mv original-data/*20* group3/
