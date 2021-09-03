07050d845656389a55ed53bd1d18eeaf80977ebc
Riley Hales <rileyhales1@gmail.com>
2021-09-03 17:10:01 -0600

    add outliers map and better var names

--- produce_dataset.py
+++ produce_dataset.py
@@ -12,7 +12,8 @@ def produce_netcdf(save_dir: str,
                    pdsi_org_table: str,
                    pdsi_ext_table: str,
                    pdsi_mod_table: str,
-                   pdsi_new_table: str, ):
+                   pdsi_new_table: str,
+                   outlier_table: str, ):
     new_nc = nc.Dataset(os.path.join(save_dir, 'pdsi_extended.nc4'), 'w', format="NETCDF4")
 
     cell_df = pd.read_csv(cell_table, index_col=0)
@@ -23,6 +24,12 @@ def produce_netcdf(save_dir: str,
 
     stats_df = pd.read_csv(stats_table, index_col=0)
     cell_df = stats_df.join(cell_df)
+
+    pdsi_out_df = pd.read_csv(outlier_table)
+    pdsi_out_df.index = pdsi_out_df['Cell']
+    del pdsi_out_df['Cell']
+    pdsi_out_df.index = pdsi_out_df.index.str.replace('c', '').astype(int)
+    cell_df = cell_df.join(pdsi_out_df)
 
     pdsi_org_df = pd.read_pickle(pdsi_org_table)
     pdsi_org_df.columns = [str(i).replace('v00_', '') for i in pdsi_org_df.columns]
@@ -60,6 +67,7 @@ def produce_netcdf(save_dir: str,
     new_nc.createVariable("pdsi_filled", 'f4', ('time', 'lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
     new_nc.createVariable("pdsi_modeled", 'f4', ('time', 'lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
     new_nc.createVariable("pdsi_new", 'f4', ('time', 'lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
+    new_nc.createVariable("pdsi_outliers", 'f4', ('lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
     new_nc.createVariable("pdsi_avg", 'f4', ('lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
     new_nc.createVariable("pdsi_std", 'f4', ('lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
     new_nc.createVariable('me', 'f4', ('lat', 'lon'), zlib=True, shuffle=True, fill_value=np.nan)
@@ -70,11 +78,12 @@ def produce_netcdf(save_dir: str,
     new_nc['pdsi_filled'].setncattr('long_name', "PDSI (Filled Gaps) + New Extension (1948-2020)")
     new_nc['pdsi_modeled'].setncattr('long_name', "PDSI Modeled by Regression (1958-2020)")
     new_nc['pdsi_new'].setncattr('long_name', "PDSI New Extension (2019-2020)")
+    new_nc['pdsi_outliers'].setncattr('long_name', "Cells with PDSI over +/-10 in 1948-2018")
     new_nc['pdsi_avg'].setncattr('long_name', "PDSI Average of 1948-2018")
     new_nc['pdsi_std'].setncattr('long_name', "PDSI Standard Deviation of 1948-2018")
-    new_nc['me'].setncattr('long_name', "Mean Error")
-    new_nc['mse'].setncattr('long_name', "Mean Square Error")
-    new_nc['rmse'].setncattr('long_name', "Root Mean Square Error")
+    new_nc['me'].setncattr('long_name', "Mean Error of PDSI Modeled 1948-2018")
+    new_nc['mse'].setncattr('long_name', "Mean Square Error of PDSI Modeled 1948-2018")
+    new_nc['rmse'].setncattr('long_name', "Root Mean Square Error of PDSI Modeled 1948-2018")
 
     # assign the time variable
     time[:] = [(i - pdsi_ext_df.index[0]).days for i in pdsi_ext_df.index]
@@ -104,11 +113,12 @@ def produce_netcdf(save_dir: str,
     new_nc['mse'][:] = me_array
     new_nc['rmse'][:] = rmse_array
 
-    # write the pdsi variables
+    # write the PDSI variables
     pdsi_ext_array = np.array([np.nan] * t_size_ext * y_size * x_size).reshape((t_size_ext, y_size, x_size))
     pdsi_org_array = np.array([np.nan] * t_size_org * y_size * x_size).reshape((t_size_org, y_size, x_size))
     pdsi_mod_array = np.array([np.nan] * t_size_mod * y_size * x_size).reshape((t_size_mod, y_size, x_size))
     pdsi_new_array = np.array([np.nan] * t_size_new * y_size * x_size).reshape((t_size_new, y_size, x_size))
+    pdsi_out_array = np.array([np.nan] * y_size * x_size).reshape((y_size, x_size))
     pdsi_avg_array = np.array([np.nan] * y_size * x_size).reshape((y_size, x_size))
     pdsi_std_array = np.array([np.nan] * y_size * x_size).reshape((y_size, x_size))
     for i, cell in enumerate(pdsi_ext_df.columns):
@@ -118,6 +128,7 @@ def produce_netcdf(save_dir: str,
             pdsi_org_array[:, row.y_idx, row.x_idx] = pdsi_org_df[cell].values.reshape(t_size_org, 1)
             pdsi_mod_array[:, row.y_idx, row.x_idx] = pdsi_mod_df[cell].values.reshape(t_size_mod, 1)
             pdsi_new_array[:, row.y_idx, row.x_idx] = pdsi_new_df[cell].values.reshape(t_size_new, 1)
+            pdsi_out_array[row.y_idx, row.x_idx] = row.outlier
             pdsi_avg_array[row.y_idx, row.x_idx] = row.pdsi_avg
             pdsi_std_array[row.y_idx, row.x_idx] = row.pdsi_std
         except:
@@ -130,6 +141,7 @@ def produce_netcdf(save_dir: str,
     new_nc['pdsi_extended'][:] = pdsi_ext_array
     new_nc['pdsi_filled'][:] = pdsi_fil_array
     new_nc['pdsi_modeled'][:] = pdsi_mod_array
+    new_nc['pdsi_outliers'][:] = pdsi_out_array
     new_nc['pdsi_new'][:] = pdsi_new_array
     new_nc['pdsi_avg'][:] = pdsi_avg_array
     new_nc['pdsi_std'][:] = pdsi_std_array
@@ -147,5 +159,7 @@ pdsi_org_table = '/Users/rchales/code/extend-pdsi/timeseries_tables_pickle_new/v
 pdsi_ext_table = '/Users/rchales/data/pdsi_data/PDSI_results_8_24/PDSI_Extended1948_2021.pickle'
 pdsi_mod_table = '/Users/rchales/data/pdsi_data/PDSI_results_8_24/PDSI_Predictions1958_2021.pickle'
 pdsi_new_table = '/Users/rchales/data/pdsi_data/PDSI_results_8_24/PDSI_Predictions2019_2021.pickle'
+outlier_table = '/Users/rchales/data/pdsi_data/PDSI_results_8_24/pdsi_outliers.csv'
 
-produce_netcdf(save_dir, cell_table, error_table, stats_table, pdsi_org_table, pdsi_ext_table, pdsi_mod_table, pdsi_new_table)
+produce_netcdf(save_dir, cell_table, error_table, stats_table, pdsi_org_table, pdsi_ext_table, pdsi_mod_table,
+               pdsi_new_table, outlier_table)
